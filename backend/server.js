e4717c75142dae925c86338558aa2dc2cc0083f4
Xintong Zhu <arielzhu@xintongs-mbp.myfiosgateway.com>
2021-04-01 17:56:20 -0400

    initialize the project

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+    secret: 'add session secret here!',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+const uri = 'mongodb://localhost/cs-online-courses';
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
